033f1feda1645058539159420b63f259c3747a7f
IlijaTrifke <gtrifunovic05@gmail.com>
2024-02-08 14:36:19 +0100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="main-nav">
+    <RouterLink :to="{ name: 'home' }">Projects</RouterLink>
+    <RouterLink :to="{ name: 'addproject' }">Add a New Project</RouterLink>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-nav {
+  text-align: center;
+  margin: 40px auto;
+}
+.main-nav a {
+  display: inline-block;
+  text-decoration: none;
+  margin: 0 10px;
+  color: #5f5a5a;
+  font-size: 18px;
+}
+a.router-link-active {
+  border-bottom: 2px solid #00ce89;
+  padding-bottom: 4px;
+}
+</style>
